d07dec3a081acd903069c43d11d84682083591ef
Mischa79 <magaleas79@gmail.com>
2023-11-14 02:30:40 +0100

    service

--- src/main/java/de/telran/URL_Shortener/URL_Shortener/controller/UrlController.java
+++ src/main/java/de/telran/URL_Shortener/URL_Shortener/controller/UrlController.java
@@ -1,10 +1,6 @@
 package de.telran.URL_Shortener.URL_Shortener.controller;
 
-
-import com.google.common.hash.HashFunction;
-import com.google.common.hash.Hashing;
-import de.telran.URL_Shortener.URL_Shortener.model.Url;
-import de.telran.URL_Shortener.URL_Shortener.repository.UrlRepository;
+import de.telran.URL_Shortener.URL_Shortener.service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,51 +8,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.nio.charset.Charset;
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Controller
 public class UrlController {
-    private UrlRepository urlRepository;
 
-
-    static HashFunction murmur = Hashing.murmur3_32_fixed(123);
+    private UrlService urlService;
 
     @Autowired
-    public UrlController(UrlRepository repository) {
-        this.urlRepository = repository;
+    public UrlController(UrlService urlService) {
+        this.urlService = urlService;
     }
 
-    //GET http://localhost:8080/go/123
     @GetMapping("/go/{shortUrl}")
     public String redirect(@PathVariable String shortUrl) {
-        List<Url> urls = urlRepository.findByShortLink(shortUrl);
-        if (urls.size() > 0)
-            return "redirect:" + urls.get(0).getOriginalUrl();
-
-        return "redirect:index.html";
+        String redirectUrl = urlService.getRedirectUrl(shortUrl);
+        return "redirect:" + redirectUrl;
     }
 
-    //http://localhost:8080/create -> create_url.html
     @GetMapping("/create")
     public String create(
-            @RequestParam (name = "url") String longUrl,
+            @RequestParam(name = "url") String longUrl,
             Model model
     ) {
-        // получить короткую ссылку по длинной ссылке
-        // localhost:8080/go/7837c903
-        String shortUrl = murmur.hashString(longUrl, Charset.defaultCharset()).toString();
-        // shortUrl = "http://localhost:8080/go/" + shortUrl;
-        Url url = new Url(longUrl, shortUrl, LocalDateTime.now());
-
-        urlRepository.save(url);
-
-        model.addAttribute("SHORT_URL", "http://localhost:8080/go/" + shortUrl);
+        String shortUrl = urlService.createShortUrl(longUrl);
+        model.addAttribute("SHORT_URL", shortUrl);
         model.addAttribute("LONG_URL", longUrl);
-
         return "create_url";
     }
-    // https://dev.mysql.com/downloads/mysql/
-
 }
---
+++ src/main/java/de/telran/URL_Shortener/URL_Shortener/service/UrlService.java
@@ -0,0 +1,35 @@
+package de.telran.URL_Shortener.URL_Shortener.service;
+
+import com.google.common.hash.Hashing;
+import de.telran.URL_Shortener.URL_Shortener.model.Url;
+import de.telran.URL_Shortener.URL_Shortener.repository.UrlRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.Charset;
+import java.time.LocalDateTime;
+import java.util.List;
+@Service
+public class UrlService {
+    private UrlRepository urlRepository;
+
+    @Autowired
+    public UrlService(UrlRepository repository) {
+        this.urlRepository = repository;
+    }
+
+    public String getRedirectUrl(String shortUrl) {
+        List<Url> urls = urlRepository.findByShortLink(shortUrl);
+        if (urls.size() > 0)
+            return urls.get(0).getOriginalUrl();
+        return "redirect:index.html";
+    }
+
+    public String createShortUrl(String longUrl) {
+        String shortUrl = Hashing.murmur3_32_fixed(123).hashString(longUrl, Charset.defaultCharset()).toString();
+        Url url = new Url(longUrl, shortUrl, LocalDateTime.now());
+        urlRepository.save(url);
+        return "http://localhost:8080/go/" + shortUrl;
+    }
+}
+
